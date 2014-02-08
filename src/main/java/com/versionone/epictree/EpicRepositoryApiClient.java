84a7393763c314c99d7c62efc323a9172085a7a9
Ian Buchanan <ian.buchanan@versionone.com>
2014-02-08 14:32:13 -0500

    detect a new epic as a change since most recent change date

--- src/main/java/com/versionone/epictree/EpicRepositoryApiClient.java
+++ src/main/java/com/versionone/epictree/EpicRepositoryApiClient.java
@@ -1,15 +1,19 @@
 package com.versionone.epictree;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import com.versionone.DB.DateTime;
 import com.versionone.apiclient.*;
 
 public class EpicRepositoryApiClient implements IEpicRepository {
-	
+
 	private DateTime mostRecentChangeDateTime;
 	private EnvironmentContext cx;
 	private IAssetType epicType;
 	private IAttributeDefinition changeAttribute;
-	
+	private static final DateFormat V1STYLE = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+
 	public EpicRepositoryApiClient(EnvironmentContext cx) {
 		this.cx = cx;
 		mostRecentChangeDateTime = null;
@@ -18,15 +22,34 @@ public class EpicRepositoryApiClient implements IEpicRepository {
 	}
 
 	public boolean isDirty() throws EpicRepositoryException {
-		if (null==mostRecentChangeDateTime) {
+		if (null == mostRecentChangeDateTime) {
 			return true;
 		}
 		return false;
-    }
+	}
 
 	public Query buildQueryForEpics() {
 		Query query = new Query(epicType);
 		query.getSelection().add(changeAttribute);
 		return query;
+	}
+
+	public boolean areThereChangedEpicsAfter(DateTime thisDate) throws EpicRepositoryException {
+		Query query = new Query(epicType);
+		query.getSelection().add(changeAttribute);
+		FilterTerm term = new FilterTerm(changeAttribute);
+		term.greater(V1STYLE.format(thisDate.getValue()));
+		query.setFilter(term);
+		QueryResult result;
+		try {
+			result = cx.getServices().retrieve(query);
+		} catch (ConnectionException e) {
+			throw new EpicRepositoryException(e);
+		} catch (APIException e) {
+			throw new EpicRepositoryException(e);
+		} catch (OidException e) {
+			throw new EpicRepositoryException(e);
+		}
+		return result.getTotalAvaliable() > 0;
 	}
 }
--- src/test/java/com/versionone/epictree/TestEpicRepository.java
+++ src/test/java/com/versionone/epictree/TestEpicRepository.java
@@ -5,22 +5,126 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.versionone.DB.DateTime;
+import com.versionone.Duration;
+import com.versionone.Oid;
 import com.versionone.apiclient.*;
 
 public class TestEpicRepository {
 	
 	private EnvironmentContext cx;
-	
+	private IAssetType epicType;
+	private IAttributeDefinition changeAttribute;
+	private Oid topProject;
+	private Asset aSchedule;
+	private Asset myTestProject;
+
 	@Before
 	public void setupVersionOneInstance() {
-		// Given a connection to a VersionOne instance
 		try {
+			// Given a connection to a VersionOne instance
 			cx = new EnvironmentContext();
+			epicType = cx.getMetaModel().getAssetType("Epic");
+			changeAttribute = epicType.getAttributeDefinition("ChangeDateUTC");
+			// And any top level project
+			topProject = getAnyTopLevelProject();
+			// And an arbitrary schedule
+			aSchedule = createNewArbitrarySchedule();
+			// And an arbitrary project under the top level with the arbitrary schedule
+			myTestProject = createNewArbitraryProject(topProject, aSchedule);			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	private DateTime findMostRecentChangeDate(Query q) {
+		DateTime mostRecentChangeDateTime = null;
+		QueryResult result = null;
+		try {
+			result = cx.getServices().retrieve(q);
+			for (Asset asset : result.getAssets()) {
+				DateTime changeDateTime = null;
+				changeDateTime = new DateTime(asset.getAttribute(changeAttribute).getValue());
+				if ((null==mostRecentChangeDateTime) || (changeDateTime.compareTo(mostRecentChangeDateTime) > 0))
+				{
+					mostRecentChangeDateTime = changeDateTime;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return mostRecentChangeDateTime;
+	}	
+	
+	private Oid getAnyTopLevelProject() {
+		Oid rootProjectId = null;
+		try {
+			// Assumption: administrative user with access to System (All Projects)
+			// TODO query to get a top-most project
+			rootProjectId = Oid.fromToken("Scope:0", cx.getMetaModel());
+		} catch (OidException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rootProjectId;
+	}
+	
+	private Asset createNewArbitrarySchedule() {
+		IAssetType scheduleType = cx.getMetaModel().getAssetType("Schedule");
+		IAttributeDefinition nameAttr = scheduleType.getAttributeDefinition("Name");
+		IAttributeDefinition timeboxGapAttr = scheduleType.getAttributeDefinition("TimeboxGap");
+		IAttributeDefinition timeboxLengthAttr = scheduleType.getAttributeDefinition("TimeboxLength");
+		Asset newSchedule = null;
+		try {
+			newSchedule = cx.getServices().createNew(scheduleType, getAnyTopLevelProject());
+			newSchedule.setAttributeValue(nameAttr, "EpicRepository Test Schedule");
+			newSchedule.setAttributeValue(timeboxLengthAttr, Duration.parse("7 Days"));
+			newSchedule.setAttributeValue(timeboxGapAttr, Duration.parse("0 Days"));
+			cx.getServices().save(newSchedule);
+		} catch (V1Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newSchedule;
+	}
+
+	private Asset createNewArbitraryProject(Oid parentProject, Asset schedule) {
+		IAssetType projectType = cx.getMetaModel().getAssetType("Scope");
+		IAttributeDefinition nameAttr = projectType.getAttributeDefinition("Name");
+		IAttributeDefinition parentAttr = projectType.getAttributeDefinition("Parent");
+		IAttributeDefinition beginDateAttr = projectType.getAttributeDefinition("BeginDate");
+		IAttributeDefinition scheduleAttr = projectType.getAttributeDefinition("Schedule");
+		Asset newProject = null;
+		try {
+			newProject = cx.getServices().createNew(projectType, parentProject);
+			newProject.setAttributeValue(nameAttr, "EpicRepository Test Project");
+			newProject.setAttributeValue(parentAttr, parentProject);
+			newProject.setAttributeValue(scheduleAttr, schedule.getOid());
+			newProject.setAttributeValue(beginDateAttr, DateTime.now());
+			cx.getServices().save(newProject);
+		} catch (V1Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newProject;
+	}
+
+	private Oid createNewEpic(Asset parentProject, String name) {
+		IAssetType epicType = cx.getMetaModel().getAssetType("Epic");
+		IAttributeDefinition nameAttr = epicType.getAttributeDefinition("Name");
+		Asset newEpic = null;
+		try {
+			newEpic = cx.getServices().createNew(epicType, parentProject.getOid());
+			newEpic.setAttributeValue(nameAttr, name);
+			cx.getServices().save(newEpic);
+		} catch (V1Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newEpic.getOid();
+	}
+
 	
 	@Test
 	public void new_repository_is_dirty() {
@@ -59,4 +163,23 @@ public class TestEpicRepository {
 		// Then the query selects the ChangeDateUTC attribute
 		assertTrue(query.getSelection().contains(changeAttribute));
 	}
+	
+	@Test
+	public void detect_a_new_epic_as_a_change_since_change_date() {
+        // Given a new repository with the connection
+		EpicRepositoryApiClient repository = new EpicRepositoryApiClient(cx);
+		// And the most recent change date from all the epics
+		DateTime mostRecentChange = findMostRecentChangeDate(repository.buildQueryForEpics());
+		// When I add a new epic
+		createNewEpic(myTestProject, "New Epic");
+        // Then a change is detected
+		boolean hasChanged = false;
+		try {
+			hasChanged = repository.areThereChangedEpicsAfter(mostRecentChange);
+		} catch (EpicRepositoryException e) {
+			fail(e.getMessage());
+		}
+		assertTrue(hasChanged);
+	}
+
 }
